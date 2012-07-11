f10844325ebe1b6db137533577a02cabbbb86f4e
nathanduffy <nathan.duffy@gmail.com>
2012-07-11 10:16:03 -0700

    Update master

--- js/data.js
+++ js/data.js
@@ -1,82 +1,82 @@
 var nflMatches = [
 	{
-		"homeTeam": {"selected": false, "displayName": "Arizona Cardinals", "image": "pictures/cardinals.gif", "conference": "AFC West", "record": "8-8"}, 
-		"awayTeam": {"selected": false, "displayName": "Carolina Panthers", "image": "pictures/panthers.gif", "conference": "NFC South", "record": "6-10"},
+		"homeTeam": {"selected": false, "displayName": "Arizona Cardinals", "image": "logos/cardinals.gif", "conference": "AFC West", "record": "8-8"}, 
+		"awayTeam": {"selected": false, "displayName": "Carolina Panthers", "image": "logos/panthers.gif", "conference": "NFC South", "record": "6-10"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Atlanta Falcons", "image": "pictures/falcons.png", "conference": "NFC South", "record": "10-6"},
-		"awayTeam": {"selected": false, "displayName": "Baltimore Ravens", "image": "pictures/ravens.gif", "conference": "AFC North", "record": "12-4"},
+		"homeTeam": {"selected": false, "displayName": "Atlanta Falcons", "image": "logos/falcons.png", "conference": "NFC South", "record": "10-6"},
+		"awayTeam": {"selected": false, "displayName": "Baltimore Ravens", "image": "logos/ravens.gif", "conference": "AFC North", "record": "12-4"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Buffalo Bills", "image": "pictures/bills.gif", "conference": "AFC East", "record": "6-10"},
-		"awayTeam": {"selected": false, "displayName": "Chicago Bears", "image": "pictures/bears.gif", "conference": "NFC North", "record": "8-8"},
+		"homeTeam": {"selected": false, "displayName": "Buffalo Bills", "image": "logos/bills.gif", "conference": "AFC East", "record": "6-10"},
+		"awayTeam": {"selected": false, "displayName": "Chicago Bears", "image": "logos/bears.gif", "conference": "NFC North", "record": "8-8"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Cincinnati Bengals", "image": "pictures/bengals.gif", "conference": "AFC North", "record": "9-7"},
-		"awayTeam": {"selected": false, "displayName": "Cleveland Browns", "image": "pictures/browns.gif", "conference": "AFC North", "record": "4-12"},
+		"homeTeam": {"selected": false, "displayName": "Cincinnati Bengals", "image": "logos/bengals.gif", "conference": "AFC North", "record": "9-7"},
+		"awayTeam": {"selected": false, "displayName": "Cleveland Browns", "image": "logos/browns.gif", "conference": "AFC North", "record": "4-12"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Dallas Cowboys", "image": "pictures/cowboys.gif", "conference": "NFC East", "record": "8-8"},
-		"awayTeam": {"selected": false, "displayName": "Denver Broncos", "image": "pictures/broncos.gif", "conference": "AFC West", "record": "8-8"},
+		"homeTeam": {"selected": false, "displayName": "Dallas Cowboys", "image": "logos/cowboys.gif", "conference": "NFC East", "record": "8-8"},
+		"awayTeam": {"selected": false, "displayName": "Denver Broncos", "image": "logos/broncos.gif", "conference": "AFC West", "record": "8-8"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Detroit Lions", "image": "pictures/lions.gif", "conference": "NFC North", "record": "10-6"},
-		"awayTeam": {"selected": false, "displayName": "Green Bay Packers", "image": "pictures/packers.gif", "conference": "NFC North", "record": "15-1"},
+		"homeTeam": {"selected": false, "displayName": "Detroit Lions", "image": "logos/lions.gif", "conference": "NFC North", "record": "10-6"},
+		"awayTeam": {"selected": false, "displayName": "Green Bay Packers", "image": "logos/packers.gif", "conference": "NFC North", "record": "15-1"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Houston Texans", "image": "pictures/texans.gif", "conference": "AFC South", "record": "10-6"},
-		"awayTeam": {"selected": false, "displayName": "Indianapolis Colts", "image": "pictures/colts.gif", "conference": "AFC South", "record": "2-14"},
+		"homeTeam": {"selected": false, "displayName": "Houston Texans", "image": "logos/texans.gif", "conference": "AFC South", "record": "10-6"},
+		"awayTeam": {"selected": false, "displayName": "Indianapolis Colts", "image": "logos/colts.gif", "conference": "AFC South", "record": "2-14"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Jacksonville Jaguars", "image": "pictures/jaguars.gif", "conference": "AFC South", "record": "5-11"},
-		"awayTeam": {"selected": false, "displayName": "Kansas City Chiefs", "image": "pictures/chiefs.gif", "conference": "AFC West", "record": "7-9"},
+		"homeTeam": {"selected": false, "displayName": "Jacksonville Jaguars", "image": "logos/jaguars.gif", "conference": "AFC South", "record": "5-11"},
+		"awayTeam": {"selected": false, "displayName": "Kansas City Chiefs", "image": "logos/chiefs.gif", "conference": "AFC West", "record": "7-9"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Miami Dolphins", "image": "pictures/dolphins.gif", "conference": "AFC East", "record": "6-10"},
-		"awayTeam": {"selected": false, "displayName": "Minnesota Vikings", "image": "pictures/vikings.gif", "conference": "NFC North", "record": "3-13"},
+		"homeTeam": {"selected": false, "displayName": "Miami Dolphins", "image": "logos/dolphins.gif", "conference": "AFC East", "record": "6-10"},
+		"awayTeam": {"selected": false, "displayName": "Minnesota Vikings", "image": "logos/vikings.gif", "conference": "NFC North", "record": "3-13"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "New England Patriots", "image": "pictures/patriots.gif", "conference": "AFC East", "record": "13-3"},
-		"awayTeam": {"selected": false, "displayName": "New Orleans Saints", "image": "pictures/saints.gif", "conference": "NFC South", "record": "13-3"},
+		"homeTeam": {"selected": false, "displayName": "New England Patriots", "image": "logos/patriots.gif", "conference": "AFC East", "record": "13-3"},
+		"awayTeam": {"selected": false, "displayName": "New Orleans Saints", "image": "logos/saints.gif", "conference": "NFC South", "record": "13-3"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "New York Giants", "image": "pictures/giants.gif", "conference": "NFC East", "record": "9-7"},
-		"awayTeam": {"selected": false, "displayName": "New York Jets", "image": "pictures/jets.gif", "conference": "AFC East", "record": "8-8"},
+		"homeTeam": {"selected": false, "displayName": "New York Giants", "image": "logos/giants.gif", "conference": "NFC East", "record": "9-7"},
+		"awayTeam": {"selected": false, "displayName": "New York Jets", "image": "logos/jets.gif", "conference": "AFC East", "record": "8-8"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Oakland Raiders", "image": "pictures/raiders.gif", "conference": "AFC West", "record": "8-8"},
-		"awayTeam": {"selected": false, "displayName": "Philadelphia Eagles", "image": "pictures/eagles.gif", "conference": "NFC East", "record": "8-8"},
+		"homeTeam": {"selected": false, "displayName": "Oakland Raiders", "image": "logos/raiders.gif", "conference": "AFC West", "record": "8-8"},
+		"awayTeam": {"selected": false, "displayName": "Philadelphia Eagles", "image": "logos/eagles.gif", "conference": "NFC East", "record": "8-8"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Pittsburgh Steelers", "image": "pictures/steelers.gif", "conference": "AFC North", "record": "12-4"},
-		"awayTeam": {"selected": false, "displayName": "San Diego Chargers", "image": "pictures/chargers.gif", "conference": "AFC West", "record": "8-8"},
+		"homeTeam": {"selected": false, "displayName": "Pittsburgh Steelers", "image": "logos/steelers.gif", "conference": "AFC North", "record": "12-4"},
+		"awayTeam": {"selected": false, "displayName": "San Diego Chargers", "image": "logos/chargers.gif", "conference": "AFC West", "record": "8-8"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "San Francisco 49ers", "image": "pictures/49ers.gif", "conference": "AFC West", "record": "13-3"},
-		"awayTeam": {"selected": false, "displayName": "Seattle Seahawks", "image": "pictures/seahawks.gif", "conference": "AFC West", "record": "7-9"},
+		"homeTeam": {"selected": false, "displayName": "San Francisco 49ers", "image": "logos/49ers.gif", "conference": "AFC West", "record": "13-3"},
+		"awayTeam": {"selected": false, "displayName": "Seattle Seahawks", "image": "logos/seahawks.gif", "conference": "AFC West", "record": "7-9"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "St. Louis Rams", "image": "pictures/rams.gif", "conference": "AFC West", "record": "2-14"},
-		"awayTeam": {"selected": false, "displayName": "Tampa Bay Buccaneers", "image": "pictures/buccaneers.gif", "conference": "NFC South", "record": "4-12"},
+		"homeTeam": {"selected": false, "displayName": "St. Louis Rams", "image": "logos/rams.gif", "conference": "AFC West", "record": "2-14"},
+		"awayTeam": {"selected": false, "displayName": "Tampa Bay Buccaneers", "image": "logos/buccaneers.gif", "conference": "NFC South", "record": "4-12"},
 		"homeTeamFavorite": false
 	},
 	{
-		"homeTeam": {"selected": false, "displayName": "Tennessee Titans", "image": "pictures/titans.gif", "conference": "AFC South", "record": "9-7"},
-		"awayTeam": {"selected": false, "displayName": "Washington Redskins", "image": "pictures/redskins.gif", "conference": "NFC East", "record": "5-11"},
+		"homeTeam": {"selected": false, "displayName": "Tennessee Titans", "image": "logos/titans.gif", "conference": "AFC South", "record": "9-7"},
+		"awayTeam": {"selected": false, "displayName": "Washington Redskins", "image": "logos/redskins.gif", "conference": "NFC East", "record": "5-11"},
 		"homeTeamFavorite": false
 	}
 ];
